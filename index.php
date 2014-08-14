c9865ee8fb9ba15d73bd52ee313f9e45c82c0731
mashuangshuang <msy095041118@126.com>
2014-08-14 15:40:38 +0800

    error

--- index.php
+++ index.php
@@ -2,5 +2,10 @@
 	$h = "hello world ";
 	$h1 = "hellow world";
 	$h3 = "hellow world";
+<<<<<<< Updated upstream
 	$h4 = "jing zhi shiyong kuaisu moshi hebing  这是一个bug";
+=======
+	$h4 = "jing zhi shiyong kuaisu moshi hebing";
+	$h5 = "bug 修复";
+>>>>>>> Stashed changes
 	echo $h;
